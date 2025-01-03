57f981b9a6f4fe996788b4278649e583bb137484
altair961 <altair89@list.ru>
2025-01-03 10:32:14 -0800

    GetProducts method was enabled to return a list of products got from database.

--- Skinet/API/Controllers/ProductController.cs
+++ Skinet/API/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using ClassLibrary1;
+﻿using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -7,19 +10,25 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts() 
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
         {
-            return "this will be a list of products";
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts() 
+        {
+            var products = _context.Products.ToList();
+
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
         public string GetProduct(int id)
         {
-            //return "single product";
-
-            var t = new Class1();
-            return t.Name;
+            return "single product";
         }
     }
 }
