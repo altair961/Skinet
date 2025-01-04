323c30f49c2dccb09ba443c517f91ca88ba963be
altair961 <altair89@list.ru>
2025-01-04 18:58:37 +0300

    Database was recreated. Methods were made asynchronous.

--- Skinet/API/Controllers/ProductController.cs
+++ Skinet/API/Controllers/ProductController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -18,17 +19,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts() 
+        public async Task<ActionResult<List<Product>>> GetProducts() 
         {
-            var products = _context.Products.ToList();
+            var products = await _context.Products.ToListAsync();
 
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "single product";
+            return await _context.Products.FindAsync(id);
         }
     }
 }
